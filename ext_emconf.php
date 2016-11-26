5e231b3f91626181bbedbf23ff35300a0a34739d
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2016-11-26 19:25:34 +0100

    [TASK] Initial Commit

---
+++ ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+
+/************************************************************************
+ * Extension Manager/Repository config file for ext "fritdrop_base".
+ ************************************************************************/
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Fritdrop Base',
+    'description' => '',
+    'category' => 'templates',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.2-8.99.99',
+            'bootstrap_packages' => ''
+        ),
+    ),
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Tomas Norre Mikkelsen',
+    'author_email' => 'tomasnorr@gmail.com',
+    'author_company' => 'private',
+    'version' => '1.0.0',
+);
